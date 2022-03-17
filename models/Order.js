827ffaa970bc46e5c85321f20ec4c919cc873719
Anthony Karlo Bantugan <anthonykarlo.bantugan@gmail.com>
2022-03-17 15:08:07 +0800

    updated Order Schema

--- models/Order.js
+++ models/Order.js
@@ -3,16 +3,7 @@ const mongoose = require("mongoose");
 const OrderSchema = new mongoose.Schema(
 	{
 		userId: { type: String, required: true },
-		products: [
-			{
-				productId: { type: String, required: true },
-				title: { type: String, required: true },
-				quantity: {
-					type: Number,
-					default: 1,
-				},
-			},
-		],
+		products: Array,
 		total: { type: Number, required: true },
 		address: { type: Object, required: true },
 		status: { type: String, default: "pending" },
